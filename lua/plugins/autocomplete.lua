2c4bd5e723d38f4afd5c9d58a4bc1c39da79b547
KevinBNaughton <kevinbnaughton@gmail.com>
2024-12-31 17:56:19 -0500

    Update blink selection to auto_insert from preselect, auto_show documentation, lower delay, add lazydev as source, <C-CR> additionally select_and_accept, update appearance, and modify formatting in the single file

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -26,6 +26,7 @@ return {
     },
   },
   version = '*',
+
   opts = function(_, opts)
     opts.completion = {
       keyword = {
@@ -38,19 +39,16 @@ return {
         -- After matching with regex, any characters matching this regex at the prefix will be excluded
         exclude_from_prefix_regex = '[\\-]',
       },
+
       trigger = {
         -- When true, will prefetch the completion items when entering insert mode
         prefetch_on_insert = false,
-
         -- When false, will not show the completion window automatically when in a snippet
         show_in_snippet = true,
-
         -- When true, will show the completion window after typing a character that matches the `keyword.regex`
         show_on_keyword = true,
-
         -- When true, will show the completion window after typing a trigger character
         show_on_trigger_character = true,
-
         -- LSPs can indicate when to show the completion window via trigger characters
         -- however, some LSPs (i.e. tsserver) return characters that would essentially
         -- always show the window. We block these by default.
@@ -58,38 +56,33 @@ return {
           if vim.api.nvim_get_mode().mode == 'c' then
             return {}
           end
-
           -- you can also block per filetype, for example:
           -- if vim.bo.filetype == 'markdown' then
           --   return { ' ', '\n', '\t', '.', '/', '(', '[' }
           -- end
-
           return { ' ', '\n', '\t' }
         end,
 
         -- When both this and show_on_trigger_character are true, will show the completion window
         -- when the cursor comes after a trigger character after accepting an item
         show_on_accept_on_trigger_character = true,
-
         -- When both this and show_on_trigger_character are true, will show the completion window
         -- when the cursor comes after a trigger character when entering insert mode
         show_on_insert_on_trigger_character = true,
-
         -- List of trigger characters (on top of `show_on_blocked_trigger_characters`) that won't trigger
         -- the completion window when the cursor comes after a trigger character when
         -- entering insert mode/accepting an item
         show_on_x_blocked_trigger_characters = { "'", '"', '(' },
         -- or a function, similar to show_on_blocked_trigger_character
       },
+
       list = {
         -- Maximum number of items to display
         max_items = 200,
-
         -- Controls if completion items will be selected automatically,
         -- and whether selection automatically inserts
-        selection = 'preselect',
+        selection = 'auto_insert',
         -- selection = function(ctx) return ctx.mode == 'cmdline' and 'auto_insert' or 'preselect' end,
-
         -- Controls how the completion items are selected
         -- 'preselect' will automatically select the first item in the completion list
         -- 'manual' will not select any item by default
@@ -106,6 +99,7 @@ return {
           from_top = true,
         },
       },
+
       accept = {
         -- Create an undo point when accepting a completion item
         create_undo_point = true,
@@ -131,6 +125,7 @@ return {
           },
         },
       },
+
       menu = {
         enabled = true,
         min_width = 15,
@@ -145,10 +140,8 @@ return {
         -- Which directions to show the window,
         -- falling back to the next direction when there's not enough space
         direction_priority = { 's', 'n' },
-
         -- Whether to automatically show the window when new completion items are available
         auto_show = true,
-
         -- Screen coordinates of the command line
         cmdline_position = function()
           if vim.g.ui_cmdline_pos ~= nil then
@@ -158,6 +151,7 @@ return {
           local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
           return { vim.o.lines - height, 0 }
         end,
+
         -- Controls how the completion items are rendered on the popup window
         draw = {
           -- Aligns the keyword you've typed to a component in the menu
@@ -168,10 +162,8 @@ return {
           gap = 1,
           -- Use treesitter to highlight the label text
           treesitter = {},
-
           -- Components to render, grouped by column
           columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 } },
-
           -- Definitions for possible components to render. Each defines:
           --   ellipsis: whether to add an ellipsis when truncating the text
           --   width: control the min, max and fill behavior of the component
@@ -212,12 +204,10 @@ return {
                 if ctx.label_detail then
                   table.insert(highlights, { #ctx.label, #ctx.label + #ctx.label_detail, group = 'BlinkCmpLabelDetail' })
                 end
-
                 -- characters matched on the label by the fuzzy matcher
                 for _, idx in ipairs(ctx.label_matched_indices) do
                   table.insert(highlights, { idx, idx + 1, group = 'BlinkCmpLabelMatch' })
                 end
-
                 return highlights
               end,
             },
@@ -242,9 +232,9 @@ return {
       },
       documentation = {
         -- Controls whether the documentation window will automatically show when selecting a completion item
-        auto_show = false,
+        auto_show = true,
         -- Delay before showing the documentation window
-        auto_show_delay_ms = 500,
+        auto_show_delay_ms = 300,
         -- Delay before updating the documentation window when selecting a new item,
         -- while an existing item is still visible
         update_delay_ms = 50,
@@ -277,9 +267,17 @@ return {
     -- NOTE: by default lazyvim already includes the lazydev source, so not
     -- adding it here again
     opts.sources = vim.tbl_deep_extend('force', opts.sources or {}, {
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'luasnip' },
-      --   default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot', 'luasnip', 'dadbod' },
+      default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer', 'luasnip' },
+      --   TODO - Maybe integrate dadbod
+      --   default = { 'dadbod' },
       providers = {
+        lazydev = {
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
+          fallbacks = { 'lsp', 'snippets', 'luasnip', 'buffer' },
+          -- make lazydev completions top priority (see `:h blink.cmp`)
+          score_offset = 100, -- the higher the number, the higher the priority
+        },
         lsp = {
           name = 'lsp',
           enabled = true,
@@ -297,6 +295,19 @@ return {
           min_keyword_length = 2,
           fallbacks = { 'snippets' },
           score_offset = 85, -- the higher the number, the higher the priority
+        },
+        -- TODO - Example on how to configure dadbod found in the main repo
+        -- https://github.com/kristijanhusak/vim-dadbod-completion
+        -- dadbod = {
+        --   name = 'Dadbod',
+        --   module = 'vim_dadbod_completion.blink',
+        --   score_offset = 83, -- the higher the number, the higher the priority
+        -- },
+        snippets = {
+          name = 'snippets',
+          enabled = true,
+          module = 'blink.cmp.sources.snippets',
+          score_offset = 80, -- the higher the number, the higher the priority
         },
         path = {
           name = 'Path',
@@ -320,19 +331,6 @@ return {
           module = 'blink.cmp.sources.buffer',
           min_keyword_length = 2,
         },
-        snippets = {
-          name = 'snippets',
-          enabled = true,
-          module = 'blink.cmp.sources.snippets',
-          score_offset = 80, -- the higher the number, the higher the priority
-        },
-        -- Example on how to configure dadbod found in the main repo
-        -- https://github.com/kristijanhusak/vim-dadbod-completion
-        -- dadbod = {
-        --   name = 'Dadbod',
-        --   module = 'vim_dadbod_completion.blink',
-        --   score_offset = 85, -- the higher the number, the higher the priority
-        -- },
       },
     })
 
@@ -364,6 +362,9 @@ return {
       ['<Tab>'] = { 'snippet_forward', 'fallback' },
       ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
 
+      ['<C-y>'] = { 'select_and_accept' },
+      ['<C-CR>'] = { 'select_and_accept' },
+
       ['<Up>'] = { 'select_prev', 'fallback' },
       ['<Down>'] = { 'select_next', 'fallback' },
       ['<C-p>'] = { 'select_prev', 'fallback' },
@@ -376,10 +377,10 @@ return {
       ['<C-e>'] = { 'hide', 'fallback' },
     }
 
-    -- opts.appearance = {
-    --  use_nvim_cmp_as_default = true,
-    --  nerd_font_variant = 'mono',
-    --}
+    opts.appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = 'mono',
+    }
 
     return opts
   end,
